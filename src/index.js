edc65f9b3fe9047d7a3e978cf8762090ed748097
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-01 13:39:31 -0500

    config mongo DB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import configExpress from './config/express.js';
+import configDb from './config/database.js';
 import routes from './routes.js';
 
 dotenv.config();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 configExpress(app);
+configDb();
 routes(app);
 
 app.listen(port, () => {
